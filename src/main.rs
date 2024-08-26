1e690d8429ebf353c1b4f173a6aa59a321154a2f
mark on github <markongithub@users.noreply.github.com>
2024-08-25 20:25:29 -0400

    I did a bunch of stuff

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use leptos::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount_to_body(|| view! { <App/> })
+    leptos::mount_to_body(|| view! { <App /> })
 }
 
 #[component]
@@ -12,16 +12,23 @@ fn App() -> impl IntoView {
     view! {
         <button
             on:click=move |_| {
-                // on stable, this is set_count.set(3);
-                // set(n) just does that
-                // update(lambda) calls a function on the existing value
                 set_count.update(|n| *n += 1);
             }
+            // the class: syntax reactively updates a single class
+            // here, we'll set the `red` class when `count` is odd
+            class:red=move || count.get() % 2 == 1
         >
+
             "Click me: "
             // on stable, this is move || count.get();
             {move || count.get()}
         </button>
+        <progress
+            max="50"
+            // signals are functions, so `value=count` and `value=move || count.get()`
+            // are interchangeable.
+            value=move || count.get()
+        />
         <p>
             <strong>"Reactive: "</strong>
             // you can insert Rust expressions as values in the DOM
@@ -31,9 +38,9 @@ fn App() -> impl IntoView {
         </p>
         <p>
             <strong>"Reactive shorthand: won't work"</strong>
-            // signals are functions, so we can remove the wrapping closure
-            // I'm not using nightly so signals are NOT functions, fuck off
-            // {count.get}
+        // signals are functions, so we can remove the wrapping closure
+        // I'm not using nightly so signals are NOT functions, fuck off
+        // {count.get}
         </p>
         <p>
             <strong>"Not reactive: "</strong>
@@ -42,4 +49,25 @@ fn App() -> impl IntoView {
             {count.get()}
         </p>
     }
+        /*
+    let (x, set_x) = create_signal(0);
+    view! {
+        <button
+            on:click=move |_| {
+                set_x.update(|n| *n += 10);
+            }
+            // set the `style` attribute
+            style="position: absolute"
+            // and toggle individual CSS properties with `style:`
+            style:left=move || format!("{}px", x.get() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", x.get(), 100)
+            style:max-width="400px"
+            // Set a CSS variable for stylesheet use
+            style=("--columns", x)
+        >
+            "Click to Move "
+            {move || x.get()}
+        </button>
+    }
+*/
 }
